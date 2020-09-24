af12d72fded9b6a23e341b57d74a183d4f56f971
Sakshi <jainsakshi381@gmail.com>
2020-09-24 11:23:59 +0530

    Informal Events

---
+++ Informal-master/js/modalh.js
@@ -0,0 +1,41 @@
+function vwx(){
+    document.getElementById("popup-12").classList.toggle("active");
+}
+function stu(){
+    document.getElementById("popup-11").classList.toggle("active");
+}
+function pqr(){
+    document.getElementById("popup-10").classList.toggle("active");
+}
+function mno(){
+    document.getElementById("popup-9").classList.toggle("active");
+}
+function jkl(){
+    document.getElementById("popup-8").classList.toggle("active");
+}
+function ghi(){
+    document.getElementById("popup-7").classList.toggle("active");
+}
+function def(){
+    document.getElementById("popup-6").classList.toggle("active");
+}
+
+function abc(){
+    document.getElementById("popup-5").classList.toggle("active");
+}
+
+function yrsza(){
+    document.getElementById("popup-4").classList.toggle("active");
+}
+
+function lkmbh(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
+function hgsrvm(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function uijht(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
