eb4c27520862e5c785c5bc07ec015cc4e864460d
Lisa Chandra <liza761c@gmail.com>
2020-09-30 00:25:43 +0530

    added another class for nav on phone

--- innerve-front-page-master/final-innerve/combined-Nikita-Amisha/app-nav.js
+++ innerve-front-page-master/final-innerve/combined-Nikita-Amisha/app-nav.js
@@ -1,6 +1,8 @@
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const container = document.querySelector(".container-main");
+const links = document.querySelector(".links");
 
 hamburger_menu.addEventListener("click", () => {
   container.classList.toggle("active");
+  links.classList.toggle("hello");
 });
